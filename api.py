ccc4769f4b55980e3db220e98f5850665be52e44
Gabritorre <gabritorredev@gmail.com>
2024-07-27 18:03:43 +0200

    Add some api

--- api.py
+++ api.py
@@ -1,16 +1,44 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from config import db
 from schemes import *
 from models import *
 
-api = Blueprint("user", __name__, url_prefix="/api")
+api = Blueprint("api", __name__, url_prefix="/api")
 
-@api.route("/user/<name>", methods=["GET"])
-def get_user(name):
+@api.route("/profile", methods=["GET"])
+def get_user():
+	user_id = request.json["id"]
+	data = db.session.get(User, user_id)
+	if not data:
+		return {"error": "User not found", "data": None}
+	
+	return {"error": None, "data": user_schema.dump(data)}
 
-	new_profile = Profile(password="1234", email="gigi123@asdf.com")
-	new_user = User(user_handle="myhandle", name=name, surname=name + "Franchetti", gender=GenderEnum.MALE, followers=0, following=0, profile=new_profile)
+@api.route("/profile/picture", methods=["GET"])
+def get_user_picture():
+	user_id = request.json["id"]
+	data = db.session.get(User, user_id)
+	if not data:
+		return {"error": "User not found", "data": None}
+	
+	return {"error": None, "data": {"pfp": user_schema.dump(data)["pfp"]}}
+
+@api.route("/search", methods=["GET"])
+def search_user():
+	user_handle = request.json["query"]
+	data = db.session.query(User).filter(User.user_handle.like("%{}%".format(user_handle))).all()
+	if not data:
+		return {"error": "No users found", "data": None}
+	
+	return {"error": None, "data": id_username_schema.dump(data)}
+
+"""
+@api.route("/create_user/<name>", methods=["GET"])
+def create_user(name):
+	new_profile = Profile(password="1234", email="oloui@asdf.com")
+	new_user = User(user_handle="gigino", name=name, surname=name + "Fran", gender=GenderEnum.MALE, followers=0, following=0, profile=new_profile)
 	db.session.add(new_profile)
 	db.session.add(new_user)
 	db.session.commit()
 	return user_schema.dump(new_user)
+"""
--- schemes.py
+++ schemes.py
@@ -11,6 +11,13 @@ class UserSchema(ma.SQLAlchemyAutoSchema):
         model = User
         include_fk = True
 
+class UserIdUsernameSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = User
+    id = ma.auto_field()
+    user_handle = ma.auto_field()
 
 profile_schema = ProfileSchema()
 user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+id_username_schema = UserIdUsernameSchema(many=True)
