5a027c2dea770f20ef8c07b6ef7bc1602a2f1b0e
M3nny <m3nny@protonmail.com>
2024-08-07 18:07:31 +0200

    Add create_campaign api

--- api/__init__.py
+++ api/__init__.py
@@ -4,6 +4,7 @@ from .user import user
 from .settings import settings
 from .feed import feed
 from .post import post
+from .adv import adv
 
 api = Blueprint("api", __name__, url_prefix="/api")
 api.register_blueprint(profile)
@@ -11,3 +12,4 @@ api.register_blueprint(user)
 api.register_blueprint(settings)
 api.register_blueprint(feed)
 api.register_blueprint(post)
+api.register_blueprint(adv)
---
+++ api/adv.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, jsonify, request
+from config import db
+from models import DATE_FORMAT, AdCampaign, Advertiser, AuthToken
+from datetime import datetime, timezone
+from .utils import hash_sha1
+
+adv = Blueprint("adv", __name__, url_prefix="/adv")
+
+@adv.route("/create_campaign", methods=["POST"])
+def create_campaign():
+	if ("auth_token" in request.cookies):
+		token = db.session.query(AuthToken).where(AuthToken.value == hash_sha1(str(request.cookies.get("auth_token"))), AuthToken.expiration_date > datetime.now(timezone.utc)).first()
+		if token:
+			adv = db.session.query(Advertiser).where(Advertiser.id == token.profile_id).first()
+			if adv:
+				req = request.get_json()
+				name = req.get("name")
+				budget = req.get("budget")
+				start = req.get("start")
+				end = req.get("end")
+				if name and budget and start and end:
+					if datetime.strptime(start, DATE_FORMAT) > datetime.now() and end > start:
+						db.session.add(AdCampaign(advertiser_id=adv.id, name=name, budget=budget, start=start, end=end))
+						db.session.commit()
+						return jsonify({"error": None})
+				return jsonify({"error": "Invalid Json content"})
+	return jsonify({"error": "Invalid token"})
