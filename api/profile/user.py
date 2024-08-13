7225143a8b553c050783a392a919f38bcccde974
M3nny <m3nny@protonmail.com>
2024-08-13 16:56:31 +0200

    Add APIs: create_post, delete_post, get_followers, get_followed

--- api/profile/user.py
+++ api/profile/user.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 from config import db, safeguard
-from models import Following, User, Profile
+from models import Following, Post, PostTag, User, Profile
 from schemas import user_schema, id_username_schema
 from api.utils import get_auth_token
 
@@ -76,3 +76,81 @@ def unfollow():
 			return jsonify({"error": "User not found"})
 	else:
 		return jsonify({"error": "User not found"})
+
+
+
+@user.route("/followers", methods=["POST"])
+@safeguard
+def get_followers():
+	req = request.get_json()
+	user_id = req["id"]
+
+	followers = (db.session.query(Following.follower.label("follower_id"), Profile.handle, Profile.name, User.pfp)
+			  .join(User, Following.follower == User.id).join(Profile, Profile.id == User.id)
+			  .where(Following.followed==user_id)
+			  .all())
+
+	return jsonify({
+		"followers": [{"id": follower.follower_id, "handle": follower.handle, "name": follower.name, "pfp": follower.pfp} for follower in followers],
+		"error": None
+	})
+
+
+
+@user.route("/followed", methods=["POST"])
+@safeguard
+def get_followed():
+	req = request.get_json()
+	user_id = req["id"]
+
+	followed = (db.session.query(Following.followed.label("followed_id"), Profile.handle, Profile.name, User.pfp)
+			  .join(User, Following.followed == User.id).join(Profile, Profile.id == User.id)
+			  .where(Following.follower==user_id)
+			  .all())
+
+	return jsonify({
+		"followed": [{"id": followed_user.followed_id, "handle": followed_user.handle, "name": followed_user.name, "pfp": followed_user.pfp} for followed_user in followed],
+		"error": None
+	})
+
+
+
+@user.route("/post", methods=["PUT"])
+@safeguard
+def create_post():
+	token = get_auth_token(request.cookies)
+	if not token:
+		return jsonify({"error": "Invalid token"})
+
+	req = request.get_json()
+	body = req["body"]
+	tags = req["tags"]
+	# TODO: media = req["media"]
+	if len(body) > 420:
+		return jsonify({"error": "Post body is too long"})
+
+	post = Post(user_id=token.profile_id, body=body)
+	db.session.add(post)
+	db.session.flush()
+	for tag in tags:
+		db.session.add(PostTag(post_id=post.id, tag_id=tag))
+	db.session.commit()
+	return jsonify({"error": None})
+
+
+
+@user.route("/post", methods=["DELETE"])
+@safeguard
+def delete_post():
+	token = get_auth_token(request.cookies)
+	if not token:
+		return jsonify({"error": "Invalid token"})
+
+	req = request.get_json()
+	post_id = req["id"]
+	db.session.query(Post).where(Post.id==post_id, Post.user_id==token.profile_id).delete()
+	db.session.commit()
+	return jsonify({"error": None})
+
+
+
