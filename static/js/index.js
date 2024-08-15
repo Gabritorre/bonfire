77a1bd80b171ec2220f8221959a75b22d2930f90
alek3y <alek3y@protonmail.com>
2024-08-15 17:46:12 +0200

    Partially templatize post

--- static/js/index.js
+++ static/js/index.js
@@ -4,9 +4,25 @@ document.addEventListener("alpine:init", () => {
 			body: "",
 			tags: []
 		},
-		posts: [],
+		posts: [
+			{
+				"id": 420,
+				"user_id": 1337,
+				"user_handle": "ferris",
+				"user_name": "Ferris the Crab",	// TODO: Add link to profile on profile header
+				"user_pfp": "http://web.archive.org/web/20240409133415if_/https://rustacean.net/more-crabby-things/squishable-ferris.jpg",
+				"body": `The Rules say there may exist either:
+- one or more references
+- exactly one mutable reference`,
+				"media": "https://cat-milk.github.io/Anime-Girls-Holding-Programming-Books/static/f5bcf000a3399d76c369dffce060f941/72645/Tsukishima_Shijima_The_Rust_programming_language.png",
+				"date": "1970-01-01",	// TODO: Format properly
+				"likes": 12000,	// TODO: Show truncated by K, M, B, T, ...
+				"comments": 1000,
+				"user_like": false	// TODO: Show on post
+			}
+		],
 
-		post() {
+		submit_post() {
 			let form = new FormData();
 			form.append("json", JSON.stringify({
 				body: this.draft.body,
